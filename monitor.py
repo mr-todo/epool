5f266bc1ebe38564816dd32fe7cdbcde2421d59d
Shaocong Ren <shaocong.ren@refinitiv.com>
2022-01-04 10:45:27 +0800

    updates

--- monitor.py
+++ monitor.py
@@ -15,8 +15,10 @@ from lib.logger import init_logger
 
 init_logger('log/monitor.log', 'INFO')
 import logging
+
 logger = logging.getLogger('monitor')
 import requests
+
 
 def monitor(args):
     logger.info("start monitor with args: {0}".format(args))
@@ -28,23 +30,25 @@ def monitor(args):
                         help='report interval in seconds. default: 30 (0.5min)')
 
     try:
+
         a = parser.parse_args(args)
 
         interval = a.interval
 
-        eth_balance='0'
-        zil_balance='0'
-        current_hashrate='0'
-        average_hashrate='0'
-        reported_hashrate='0'
-        online_count='0'
-        last_eth_balance='0'
-        last_zil_balance='0'
-        last_current_hashrate='0'
-        last_average_hashrate='0'
-        last_reported_hashrate='0'
-        last_online_count='0'
-        updatedTime=datetime.datetime.now()
+        eth_balance = '0'
+        current_hashrate = '0'
+        average_hashrate = '0'
+        reported_hashrate = '0'
+        online_count = '0'
+        last_eth_balance = '0'
+        last_current_hashrate = '0'
+        last_average_hashrate = '0'
+        last_reported_hashrate = '0'
+        last_online_count = '0'
+        updatedTime = datetime.datetime.now()
+        time_to_payout = '0'
+        gas_price = '0'
+
         while True:
             start_time = time.time()
 
@@ -56,7 +60,7 @@ def monitor(args):
             </table>
             <br />
             <table border=1>
-            <caption, align=left>Ezil Stats</caption>
+            <caption, align=left>Worker Stats</caption>
                 <tr>
                    <th>Item</th>
                    <th>Current Value</th>
@@ -67,10 +71,13 @@ def monitor(args):
             <br />
             <table border=1>
                 <tr><td>Updated Time</td><td>#UpdatedTime#</td></tr>
+                <tr><td>Time to Payout</td><td>#time_to_reach_min_payout#</td></tr>
+                <tr><td>Gas Price</td><td>#current_gas_price#</td></tr>
             </table>
             """
 
-            #import requests
+            # import requests
+
 
             url = "https://billing.ezil.me/balances/0xd100eA705Bf62fbB5F07daB652f52A0D2f0a1FBF"
 
@@ -79,57 +86,77 @@ def monitor(args):
 
             try:
                 response = requests.request("GET", url, headers=headers, data=payload)
+                dict = json.loads(response.text)
+
+                if eth_balance != dict['eth']:
+                    last_eth_balance = eth_balance
+                    eth_balance = dict['eth']
+                    updatedTime = datetime.datetime.now()
             except:
                 print("Oops!", sys.exc_info()[0], "occurred.")
                 continue
 
-            dict = json.loads(response.text)
-
-            if eth_balance!=dict['eth']:
-                last_eth_balance = eth_balance
-                eth_balance = dict['eth']
-                updatedTime = datetime.datetime.now()
-
-            rows = ''
-            rows = rows + '<tr><td>{0}</td><td>{1}</td><td>{2}</td></tr>'.format('ETH Balance', eth_balance,last_eth_balance)
-
-
-            url="https://stats.ezil.me/current_stats/0xd100eA705Bf62fbB5F07daB652f52A0D2f0a1FBF/workers/stats?coin=eth"
+            url = "https://billing.ezil.me/v2/accounts/0xd100eA705Bf62fbB5F07daB652f52A0D2f0a1FBF"
             try:
                 response = requests.request("GET", url, headers=headers, data=payload)
+                dict = json.loads(response.text)
+
+                if eth_balance != dict['balances'][0]['amount']:
+                    last_eth_balance = eth_balance
+                    eth_balance = dict['balances'][0]['amount']
+                    updatedTime = datetime.datetime.now()
+                time_to_payout = '{0} hours'.format(float(dict['balances'][0]['payout']['time_to_reach_min_payout']) / 3600)
+                gas_price = str(dict['balances'][0]['payout']['current_gas_price'])
             except:
                 print("Oops!", sys.exc_info()[0], "occurred.")
                 continue
 
-            dict = json.loads(response.text)
+            rows = ''
+            rows = rows + '<tr><td>{0}</td><td>{1}</td><td>{2}</td></tr>'.format('ETH Balance', eth_balance,
+                                                                                 last_eth_balance)
 
-            if current_hashrate!=dict['current_hashrate']:
-                last_current_hashrate = current_hashrate
-                current_hashrate = dict['current_hashrate']
-                updatedTime = datetime.datetime.now()
-            if average_hashrate!=dict['average_hashrate']:
-                last_average_hashrate = average_hashrate
-                average_hashrate = dict['average_hashrate']
-                updatedTime = datetime.datetime.now()
-            if reported_hashrate!=dict['reported_hashrate']:
-                last_reported_hashrate = reported_hashrate
-                reported_hashrate = dict['reported_hashrate']
-                updatedTime = datetime.datetime.now()
-            if online_count!=dict['workers']['online_count']:
-                last_online_count = online_count
-                online_count = dict['workers']['online_count']
-                updatedTime = datetime.datetime.now()
+            url = "https://stats.ezil.me/current_stats/0xd100eA705Bf62fbB5F07daB652f52A0D2f0a1FBF/workers/stats?coin=eth"
+            try:
+                response = requests.request("GET", url, headers=headers, data=payload)
+                dict = json.loads(response.text)
 
+                if current_hashrate != dict['current_hashrate']:
+                    last_current_hashrate = current_hashrate
+                    current_hashrate = dict['current_hashrate']
+                    updatedTime = datetime.datetime.now()
+                if average_hashrate != dict['average_hashrate']:
+                    last_average_hashrate = average_hashrate
+                    average_hashrate = dict['average_hashrate']
+                    updatedTime = datetime.datetime.now()
+                if reported_hashrate != dict['reported_hashrate']:
+                    last_reported_hashrate = reported_hashrate
+                    reported_hashrate = dict['reported_hashrate']
+                    updatedTime = datetime.datetime.now()
+                if online_count != dict['workers']['online_count']:
+                    last_online_count = online_count
+                    online_count = dict['workers']['online_count']
+                    updatedTime = datetime.datetime.now()
+            except:
+                print("Oops!", sys.exc_info()[0], "occurred.")
+                continue
 
-            rows = rows + '<tr><td>{0}</td><td>{1} MHz/s</td><td>{2} MHz/s</td></tr>'.format('30 min', float(current_hashrate)/1000000,float(last_current_hashrate)/1000000)
-            rows = rows + '<tr><td>{0}</td><td>{1} MHz/s</td><td>{2} MHz/s</td></tr>'.format('3 hour', float(average_hashrate)/1000000,float(last_average_hashrate)/1000000)
-            rows = rows + '<tr><td>{0}</td><td>{1} MHz/s</td><td>{2} MHz/s</td></tr>'.format('Local', float(reported_hashrate)/1000000,float(last_reported_hashrate)/1000000)
-            rows = rows + '<tr><td>{0}</td><td>{1}</td><td>{2}</td></tr>'.format('Worker Status', online_count,last_online_count)
+            rows = rows + '<tr><td>{0}</td><td>{1} MHz/s</td><td>{2} MHz/s</td></tr>'.format('30 min', float(
+                current_hashrate) / 1000000, float(last_current_hashrate) / 1000000)
+            rows = rows + '<tr><td>{0}</td><td>{1} MHz/s</td><td>{2} MHz/s</td></tr>'.format('3 hour', float(
+                average_hashrate) / 1000000, float(last_average_hashrate) / 1000000)
+            rows = rows + '<tr><td>{0}</td><td>{1} MHz/s</td><td>{2} MHz/s</td></tr>'.format('Reported', float(
+                reported_hashrate) / 1000000, float(last_reported_hashrate) / 1000000)
+            rows = rows + '<tr><td>{0}</td><td>{1}</td><td>{2}</td></tr>'.format('Worker Status', online_count,
+                                                                                 last_online_count)
+
+            html_str = html_str.replace('#rows#', rows)
+            html_str = html_str.replace('#UpdatedTime#', updatedTime.strftime('%Y-%m-%d %H:%M:%S'))
+            html_str = html_str.replace('#time_to_reach_min_payout#', time_to_payout)
+            html_str = html_str.replace('#current_gas_price#', gas_price)
 
             Html_file = open("index.html", "w")
-            Html_file.write(html_str.replace('#rows#',rows).replace('#UpdatedTime#',updatedTime.strftime('%Y-%m-%d %H:%M:%S')))
+            Html_file.write(html_str)
             Html_file.close()
-
 
             end_time = time.time()
             elapsed = end_time - start_time
